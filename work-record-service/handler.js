0aa9fda28e799aab68d59ace90768d7e181483e5
junjun <aa106921@gmail.com>
2021-12-28 00:05:52 +0900

    serverless.yml に Lambda の設定を追加

--- work-record-service/handler.js
+++ work-record-service/handler.js
@@ -1,18 +1,36 @@
-'use strict';
+const AWS = require('aws-sdk');
+const { DateTime } = require('luxon');
+const dynamo = new AWS.DynamoDB.DocumentClient({
+  apiVersion: '2012-08-10',
+  region: "us-west-2"
+});
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.putRecord = async (event) => {
+  const statusCode = {
+    OK: 200,
+    BadRequest: 400,
+    InternalServerError: 500
+  }
+  try {
+    const requestRecord = JSON.parse(JSON.stringify(event));
+    try {
+      await dynamo.put({
+        TableName: "work-record-log",
+        Item: {
+          "date": DateTime.now().toFormat('yyyy-MM-dd'),
+          "startTime": DateTime.now().toFormat('HH:mm:ss'),
+          "endTime": null,
+          "timeLength": null,
+          "content": requestRecord.content,
+          "category": requestRecord.category,
+        }
+      }).promise();
+      return statusCode.OK;
+    } catch (ex) {
+      return statusCode.InternalServerError;
+    }
+  } catch (err) {
+    console.log(err.message);
+    return statusCode.BadRequest;
+  }
+}
